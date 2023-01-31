e743168e71f78d25959085139d62fd9929bf10a0
Andrew Powers <arpowers@gmail.com>
2023-01-31 13:28:59 -0800

    fix: change hash size to 128kb

--- modules/hashes.py
+++ modules/hashes.py
@@ -36,7 +36,7 @@ def cache(subsection):
 
 def calculate_sha256(filename):
     hash_sha256 = hashlib.sha256()
-    blksize = 1024 * 1024
+    blksize = 1024 * 128
 
     with open(filename, "rb") as f:
         for chunk in iter(lambda: f.read(blksize), b""):
